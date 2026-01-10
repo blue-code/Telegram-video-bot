1e6a646aa8cbf00aff43590792b64b2eae953995
김병호 <digimaru@gmail.com>
2026-01-10 09:40:22 +0900

    Config: Enable external access to web server by binding uvicorn to 0.0.0.0

--- streaming_server.bat
+++ streaming_server.bat
@@ -3,6 +3,6 @@ SETLOCAL
 cd /d %~dp0
 set OPENSSL_CONF=openssl_legacy.cnf
 call venv_win\Scripts\activate.bat
-uvicorn src.server:app --reload --port 8000
+uvicorn src.server:app --host 0.0.0.0 --reload --port 8000
 pause
 ENDLOCAL
